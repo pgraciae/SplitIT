ea3d1344c1fc5d85cd67be0016376c2eb6bee43e
Daniel <daniel.navarroh@e-campus.uab.cat>
2021-11-23 19:06:29 +0100

    Main Page with Table

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import TableX from './Components/Table.js';
 class App extends React.Component {
 
   constructor(props){
@@ -18,17 +19,24 @@ class App extends React.Component {
     });
   }
 
+  button_test(){
+    console.log('Testing .... ')
+  }
   render(){
     
     return(
     <div className="App">
         <header className="App-header">
+        <Stack direction="row" spacing={2}>
+      <Button variant="contained" onClick ={this.button_test} style = {{position: "absolute", top: '10vw', left: '45vw', width: '10vw', height: '5vw'}}>Gallery</Button>
+      <Button variant="contained" onClick ={this.button_test} style = {{position: "absolute", top: '10vw', left: '55vw', width: '10vw', height: '5vw'}} href="#contained-buttons">
+        CAMERA
+      </Button>
+    </Stack>
+    <div style = {{position: 'absolute', width: '70vw', left: '22vw', top:'20vw'}}>
+      <TableX></TableX>
+      </div>
 
-        <button>
-          Upload Image
-        </button>
-
-        <p>The current time is {this.state.currentTime}.</p>
       </header>
     </div>
     )
---
+++ src/Components/Table.js
@@ -0,0 +1,166 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Input from "@material-ui/core/Input";
+import Paper from "@material-ui/core/Paper";
+import IconButton from "@material-ui/core/IconButton";
+// Icons
+import EditIcon from "@material-ui/icons/EditOutlined";
+import DoneIcon from "@material-ui/icons/DoneAllTwoTone";
+import RevertIcon from "@material-ui/icons/NotInterestedOutlined";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    marginTop: theme.spacing(3),
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 650
+  },
+  selectTableCell: {
+    width: 60
+  },
+  tableCell: {
+    width: 130,
+    height: 40
+  },
+  input: {
+    width: 130,
+    height: 40
+  }
+}));
+
+const createData = (Quantity, ProductName, Price) => ({
+  id: ProductName.replace(" ", "_"),
+  Quantity, ProductName, Price,isEditMode: false
+});
+
+const CustomTableCell = ({ row, name, onChange }) => {
+  const classes = useStyles();
+  const { isEditMode } = row;
+  return (
+    <TableCell align="left" className={classes.tableCell}>
+      {isEditMode ? (
+        <Input
+          value={row[name]}
+          name={name}
+          onChange={(e) => onChange(e, row)}
+          className={classes.input}
+        />
+      ) : (
+        row[name]
+      )}
+    </TableCell>
+  );
+};
+
+ function TableX() {
+  const [rows, setRows] = React.useState([
+    createData(159 , "Frozen yoghurt", 24,),
+    createData(237, "Ice cream sandwich",34),
+    createData(262 , "Eclair", 12 )
+  ]);
+  const [previous, setPrevious] = React.useState({});
+  const classes = useStyles();
+
+  const onToggleEditMode = (id) => {
+    setRows((state) => {
+      return rows.map((row) => {
+        if (row.id === id) {
+          return { ...row, isEditMode: !row.isEditMode };
+        }
+        return row;
+      });
+    });
+  };
+
+  const onChange = (e, row) => {
+    if (!previous[row.id]) {
+      setPrevious((state) => ({ ...state, [row.id]: row }));
+    }
+    const value = e.target.value;
+    const name = e.target.name;
+    const { id } = row;
+    const newRows = rows.map((row) => {
+      if (row.id === id) {
+        return { ...row, [name]: value };
+      }
+      return row;
+    });
+    setRows(newRows);
+  };
+
+  const onRevert = (id) => {
+    const newRows = rows.map((row) => {
+      if (row.id === id) {
+        return previous[id] ? previous[id] : row;
+      }
+      return row;
+    });
+    setRows(newRows);
+    setPrevious((state) => {
+      delete state[id];
+      return state;
+    });
+    onToggleEditMode(id);
+  };
+
+  return (
+    <Paper className={classes.root}>
+      <Table className={classes.table} aria-label="caption table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="left" />
+            <TableCell align="left">Quantity</TableCell>
+            <TableCell align="left">Product Name</TableCell>
+            <TableCell align="left">Price</TableCell>
+            
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.id}>
+              <TableCell className={classes.selectTableCell}>
+                {row.isEditMode ? (
+                  <>
+                    <IconButton
+                      aria-label="done"
+                      onClick={() => onToggleEditMode(row.id)}
+                    >
+                      <DoneIcon />
+                    </IconButton>
+                    <IconButton
+                      aria-label="revert"
+                      onClick={() => onRevert(row.id)}
+                    >
+                      <RevertIcon />
+                    </IconButton>
+                  </>
+                ) : (
+                  <IconButton
+                    aria-label="delete"
+                    onClick={() => onToggleEditMode(row.id)}
+                  >
+                    <EditIcon />
+                  </IconButton>
+                )}
+              </TableCell>
+              <CustomTableCell {...{ row, name: "Quantity", onChange }} />
+              <CustomTableCell {...{ row, name: "ProductName", onChange }} />
+              <CustomTableCell {...{ row, name: "Price", onChange }} />
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
+  );
+}
+
+
+export default TableX;
