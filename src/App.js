daf0471f87aed341541a161fe965aeafeba50949
pgraciae <pol.graciae@gmail.com>
2021-11-30 14:16:40 +0100

    NavBAr

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import './App.css';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import TableX from './Components/Table.js';
+import NavBarCustom from './Components/NavBar';
+import { ThumbUpSharp } from '@material-ui/icons';
+
 class App extends React.Component {
 
   constructor(props){
     super(props);
-    this.state = {currentTime: 0}
+    this.state = {currentTime: 0, View: 'Home'}
+    this.go = this.move.bind(this)
   }
 
   componentDidMount(){
@@ -19,14 +23,23 @@ class App extends React.Component {
     });
   }
 
+  move(event, name) {
+    event.preventDefault()
+    this.setState({View: name})
+  }
+
   button_test(){
     console.log('Testing .... ')
   }
   render(){
     
+
+    if (this.state.View === 'Home'){
+  
     return(
     <div className="App">
         <header className="App-header">
+      <NavBarCustom view={this.go}></NavBarCustom>
         <Stack direction="row" spacing={2}>
       <Button variant="contained" onClick ={this.button_test} style = {{position: "absolute", top: '10vw', left: '45vw', width: '10vw', height: '5vw'}}>Gallery</Button>
       <Button variant="contained" onClick ={this.button_test} style = {{position: "absolute", top: '10vw', left: '55vw', width: '10vw', height: '5vw'}} href="#contained-buttons">
@@ -36,11 +49,34 @@ class App extends React.Component {
     <div style = {{position: 'absolute', width: '70vw', left: '22vw', top:'20vw'}}>
       <TableX></TableX>
       </div>
-
       </header>
+
     </div>
     )
   }
+  else if (this.state.View === 'Profile'){
+    return(
+      <div className="App">
+      <header className="App-header">
+      <div>
+        <NavBarCustom view={this.go}></NavBarCustom>
+      </div>
+      </header>
+      </div>
+    )
+  }
+  else if (this.state.View === 'Tickets'){
+    return(
+      <div className="App">
+      <header className="App-header">
+      <div>
+        <NavBarCustom view={this.go}></NavBarCustom>
+      </div>
+      </header>
+      </div>
+    )
+  }
+}
 
 }
 
---
+++ src/Components/NavBar.js
@@ -0,0 +1,24 @@
+
+import Container from 'react-bootstrap/Container'
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css' ;
+
+function NavBarCustom(props) {
+
+    return (
+        <div style = {{position: 'absolute', width: '100%', left: '0vw', top:'0vw'}}>
+  <Navbar bg="dark" variant="dark" >
+    <Container style = {{height:'3vw'}}>
+    <Nav className="me-auto" >
+      <Nav.Link href="#home" style = {{left:'10vw',top:'0.5vw',  position:'absolute'}} onClick={(event)=>props.view(event, 'Home')}>Home</Nav.Link>
+      <Nav.Link href="#features" style = {{left:'20vw', top:'0.5vw', position:'absolute'}} onClick={(event)=>props.view(event, 'Profile')}>Profile</Nav.Link>
+      <Nav.Link href="#pricing" style = {{left:'30vw',top:'0.5vw',  position:'absolute'}} onClick={(event)=>props.view(event, 'Tickets')}>Tickets</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+  <br />
+
+ </div>    
+ )
+}
+export default NavBarCustom;
