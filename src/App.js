671506d1c65c88a23bbac4a4df1c734cb33d66ce
ferranvf <ferran.vera.filella@.gmail.com>
2021-12-02 13:43:54 +0100

    Open and close camera

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import Webcam from "react-webcam";
+import 'react-html5-camera-photo/build/css/index.css';
 import logo from './logo.svg';
 import './App.css';
 import Button from '@mui/material/Button';
@@ -8,13 +10,16 @@ import NavBarCustom from './Components/NavBar';
 import { ThumbUpSharp } from '@material-ui/icons';
 import inputFile from './Components/uploadFile';
 
+
 class App extends React.Component {
+
 
   constructor(props){
     super(props);
-    this.state = {currentTime: 0, View: 'Home', selectedFile:null}
+    this.state = {currentTime: 0, View: 'Home', selectedFile:null, webcamEnabled: false}
     this.go = this.move.bind(this)
     this.loadedImage = this.loadImage.bind(this)
+    this.enableWebcam = this.enableWebcam.bind(this)
   }
 
   componentDidMount(){
@@ -38,15 +43,24 @@ class App extends React.Component {
       this.setState({ selectedFile: event.target.files[0] }, () => console.log(this.state.selectedFile))
 
     }
+    
+    enableWebcam (){
+      console.log(this.state.webcamEnabled)
+      this.setState({webcamEnabled: !this.state.webcamEnabled});
+      console.log(this.state.webcamEnabled)
+    }
   
-
   button_test(){
     console.log('Testing .... ')
   }
   render(){
 
     if (this.state.View === 'Home'){
-  
+      const videoConstraints = {
+        width: 1280,
+        height: 720,
+        facingMode: "user"
+      };
     return(
     <div className="App">
         <header className="App-header">
@@ -61,16 +75,28 @@ class App extends React.Component {
         onChange={(event) => this.loadImage(event)}
       />
     <label htmlFor="raised-button-file">
-      <Button variant="raised" component="span" style = {{position: "absolute", top: '10vw', left: '45vw', width: '10vw', height: '5vw'}} >
+      <Button variant="contained" component="span" style = {{position: "absolute", top: '10vw', left: '35vw', width: '10vw', height: '5vw'}} >
         Upload
       </Button>
     </label>
 
-      <Button variant="contained" onClick ={this.button_test} style = {{position: "absolute", top: '10vw', left: '55vw', width: '10vw', height: '5vw'}} href="#contained-buttons">
-        CAMERA
+      <Button variant="contained" onClick ={this.button_test} style = {{position: "absolute", top: '10vw', left: '45vw', width: '10vw', height: '5vw'}}>
+        NO SABIA SI PODIA TREURE
+      </Button>
+      {this.state.webcamEnabled &&
+      <Webcam
+          audio={false}
+          height={200}
+          ref={this.setRef}
+          screenshotFormat="image/jpeg"
+          width={350}
+          videoConstraints={videoConstraints}
+        />}
+      <Button variant ="contained" onClick={this.enableWebcam} style = {{position: "absolute", top: '10vw', left: '55vw', width: '10vw', height: '5vw'}}>
+        Camera
       </Button>
     </Stack>
-    <div style = {{position: 'absolute', width: '70vw', left: '22vw', top:'20vw'}}>
+    <div style = {{position: 'absolute', width: '70vw', left: '22vw', top:'60vw'}}>
       <TableX></TableX>
       </div>
       </header>
