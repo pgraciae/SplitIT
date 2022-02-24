989f0e1f11621b0c6866bd758bd47aec2672f8a9
pgraciae <pol.graciae@gmail.com>
2022-02-24 12:21:09 +0100

    Flask Backend progress - SQLAlchemy

--- api/app.py
+++ api/app.py
@@ -1,11 +1,20 @@
 import time
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:FDP@SplitItDB:5432/postgres'
+db = SQLAlchemy(app)
+db.Model.metadata.reflect(db.engine)
+
 
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
-    
+
 if __name__ == '__main__':
         app.run(debug=True,host='0.0.0.0', port = 5000)
+        print(db.Model.metadata.tables.keys())
---
+++ api/tables.py
@@ -0,0 +1,39 @@
+from .app import db
+from flask_sqlalchemy import SQLAlchemy
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+
+class UserTable(db.Model):
+    __table__ = db.Model.metadata.tables['usertable']
+    
+    def __repr__(self):
+        return self.nickname
+
+class Friends(db.Model):
+    __table__ = db.Model.metadata.tables['friends']
+
+class Groups(db.Model):
+    __table__ = db.Model.metadata.tables['groups']
+
+class Group_User(db.Model):
+    __table__ = db.Model.metadata.tables['group_user']
+
+class Ticket(db.Model):
+    __table__ = db.Model.metadata.tables['ticket']
+
+    def __repr__(self):
+        return str(self.ticket_id)
+
+class Payment_Method(db.Model):
+    __table__ = db.Model.metadata.tables['payment_method']
+
+class Subticket(db.Model):
+    __table__ = db.Model.metadata.tables['subticket']
+
+class Product(db.Model):
+    __table__ = db.Model.metadata.tables['product']
+
+class Products_Prop(db.Model):
+    __table__ = db.Model.metadata.tables['products_prop']
