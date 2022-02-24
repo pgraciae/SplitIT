49c69c7ae325c80dd301ef8acdbb354b33281279
DeepDaniel-eng <71936360+DeepDaniel-eng@users.noreply.github.com>
2022-02-24 19:19:54 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -10,10 +10,6 @@ import NavBarCustom from './Components/NavBar';
 import { ThumbUpSharp } from '@material-ui/icons';
 import inputFile from './Components/uploadFile';
 import ProfileView from './Profile';
-import Login from "./Components/Login.js";
-//import { Switch } from '@mui/material';
-
-import { Routes, Switch, Route, Link } from "react-router-dom";
 
 class App extends React.Component {
 
@@ -25,7 +21,23 @@ class App extends React.Component {
     this.loadedImage = this.loadImage.bind(this)
     this.enableWebcam = this.enableWebcam.bind(this)
   }
-
+  handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("here")
+    const formData = new FormData();
+    formData.append('file', this.state.selectedFile);
+  
+    const Upload = async() => {
+      await fetch('/upload', {
+        method: 'POST',
+        body: formData
+      }).then(resp => {
+        resp.json().then(data => {console.log(data)})
+      })
+    }
+    
+  }
+    
   componentDidMount(){
     fetch('/time').then(res => res.json()).then(data => {
 
@@ -39,15 +51,15 @@ class App extends React.Component {
     this.setState({View: name})
   }
 
+
   loadImage(event) {
       
-      //event.preventDefault()
-      console.log(event.target.files)
-      // Update the state
-      this.setState({ selectedFile: event.target.files[0] }, () => console.log(this.state.selectedFile))
+    //event.preventDefault()
+    console.log(event.target.files)
+    // Update the state
+    this.setState({ selectedFile: event.target.files[0] }, () => console.log(this.state.selectedFile))
 
-    }
-    
+  }
     enableWebcam (){
       console.log(this.state.webcamEnabled)
       this.setState({webcamEnabled: !this.state.webcamEnabled});
@@ -58,20 +70,7 @@ class App extends React.Component {
     console.log('Testing .... ')
   }
   render(){
-    if (this.state.View === 'Login'){
-    return(
-        <div className="App">
-        <header className="App-header">
-        <div>
-          <NavBarCustom view={this.go}></NavBarCustom>
-        </div>
-        <Login>
-        </Login>
-        </header>
-  
-        </div>
-    )
-    }
+
     if (this.state.View === 'Home'){
       const videoConstraints = {
         width: 1280,
@@ -81,8 +80,9 @@ class App extends React.Component {
     return(
     <div className="App">
         <header className="App-header">
-      <NavBarCustom view={this.go}></NavBarCustom>
+        <NavBarCustom view={this.go}></NavBarCustom>
         <Stack direction="row" spacing={2}>
+      <form onSubmit = {this.handleSubmit}  >
       <input
         accept="image/*"
         style={{ display: 'none' }}
@@ -97,9 +97,9 @@ class App extends React.Component {
       </Button>
     </label>
 
-      {/* <Button variant="contained" onClick ={this.button_test} style = {{position: "absolute", top: '10vw', left: '45vw', width: '10vw', height: '5vw'}}>
+      <Button variant="contained" onClick ={this.button_test} style = {{position: "absolute", top: '10vw', left: '45vw', width: '10vw', height: '5vw'}}>
         NO SABIA SI PODIA TREURE
-    </Button> */}
+      </Button>
       {this.state.webcamEnabled &&
       <Webcam
           audio={false}
@@ -112,6 +112,10 @@ class App extends React.Component {
       <Button variant ="contained" onClick={this.enableWebcam} style = {{position: "absolute", top: '10vw', left: '55vw', width: '10vw', height: '5vw'}}>
         Camera
       </Button>
+      <Button variant ="contained" type="submit" style = {{position: "absolute", top: '20vw', left: '45vw', width: '10vw', height: '5vw'}}>
+        Submit File
+      </Button>
+      </form>
     </Stack>
     <div style = {{position: 'absolute', width: '70vw', left: '22vw', top:'60vw'}}>
       <TableX></TableX>
